3cf12017b5c7f80535afaf497e84b8e2bc0dc042
Dai Le <dai.n.le@sjsu.edu>
2020-02-16 14:55:45 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Dai Le on 2/16/20.
+//  Copyright © 2020 daile14011997@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
