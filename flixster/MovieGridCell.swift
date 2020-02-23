2c236c67410b4f83f70c708415b6c64e389c79cc
Dai Le <dai.n.le@sjsu.edu>
2020-02-23 15:06:25 -0800

    Finish Flix part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Dai Le on 2/23/20.
+//  Copyright © 2020 daile14011997@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+ 
+  
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
